f5bcaaad4b8f3da48c42c96b3066ea4a1df6abbb
kazuki <daisuki6678@outlook.jp>
2025-12-30 10:34:15 +0900

    20251230_Home画面のListViewの下にFAB分の余白を設定

--- lib/features/home/presentation/widgets/display_list.dart
+++ lib/features/home/presentation/widgets/display_list.dart
@@ -9,32 +9,32 @@ class DisplayList extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Expanded(
-      child: ListView.builder(
-        itemCount: diaries.length,
-        itemBuilder: (context, index) {
-          final diary = diaries[index];
-          return Card(
-            margin: const EdgeInsets.symmetric(horizontal: 12, vertical: 6),
-            child: ListTile(
-              leading: Icon(
-                sentimentIcon(diary.sentiment),
-                color: sentimentColor(diary.sentiment),
-                size: 30,
-              ),
-              title: Text(
-                diary.title,
-                maxLines: 2,
-                overflow: TextOverflow.ellipsis,
-              ),
-              subtitle: Text(
-                DateFormat('yyyy/MM/dd HH:mm:ss').format(diary.date.toLocal()),
-              ),
-              trailing: sentimentChip(diary.sentiment!),
+    return ListView.builder(
+      // FABの分だけ下に余白を作る
+      padding: EdgeInsets.only(bottom: 96),
+      itemCount: diaries.length,
+      itemBuilder: (context, index) {
+        final diary = diaries[index];
+        return Card(
+          margin: const EdgeInsets.symmetric(horizontal: 12, vertical: 6),
+          child: ListTile(
+            leading: Icon(
+              sentimentIcon(diary.sentiment),
+              color: sentimentColor(diary.sentiment),
+              size: 30,
             ),
-          );
-        },
-      ),
+            title: Text(
+              diary.title,
+              maxLines: 2,
+              overflow: TextOverflow.ellipsis,
+            ),
+            subtitle: Text(
+              DateFormat('yyyy/MM/dd HH:mm:ss').format(diary.date.toLocal()),
+            ),
+            trailing: sentimentChip(diary.sentiment!),
+          ),
+        );
+      },
     );
   }
 
